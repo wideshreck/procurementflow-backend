b67ddd4058a16e9c8407f4c3c990b943f17363a5
Muhammed Taşkın <muhammedtaskin.cs@gmail.com>
2025-09-07 00:20:19 +0300

    small adjustment

--- src/modules/rfx-templates/rfx-templates.module.ts
+++ src/modules/rfx-templates/rfx-templates.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RFxTemplatesService } from './rfx-templates.service';
 import { RFxTemplatesController } from './rfx-templates.controller';
 import { PrismaService } from '../../prisma/prisma.service';
-import { AIModule } from '../ai/ai.module';
+import { AIModule } from './ai/ai.module';
 
 @Module({
   imports: [AIModule],
